276b47b19fae82b0a204a22e4ed061b860a3da4d
caixiao <148125492@qq.com>
2020-01-02 17:34:06 +0800

    修改分页查询逻辑

--- Oms-Oms-controller/src/main/java/com/work/shop/oms/controller/CreateOrderController.java
+++ Oms-Oms-controller/src/main/java/com/work/shop/oms/controller/CreateOrderController.java
@@ -223,15 +223,11 @@ public class CreateOrderController extends BaseController {
         int pageCount = (int) Math.ceil(count * 1.0 / param.getPageSize());
         if (param.getCurrentPage() > 1) {
             //查询最后一页的逻辑
-            if (param.getCurrentPage() > pageCount) {
-                result.setIsOk("1");
-                result.setTotal(0);
-                return result;
-            } else if (param.getCurrentPage() == pageCount) {
+            if (param.getCurrentPage() >= pageCount) {
                 param.setStart(param.getPageSize() * (pageCount - 1));
                 param.setPageSize(count - param.getPageSize() * (pageCount - 1));
             } else {
-                param.setStart(param.getCurrentPage() * (param.getPageSize() - 1));
+                param.setStart(param.getPageSize()* (param.getCurrentPage() - 1));
             }
         } else {
             param.setStart(0);
