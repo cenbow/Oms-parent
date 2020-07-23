e531ef464735324d555395d1a174b1f18bc8211f
gujiawei <g46474816@126.com>
2020-07-23 10:20:37 +0800

    '订单详情返回团购id'

--- Oms-base-api/src/main/java/com/work/shop/oms/api/bean/OrderDetailInfo.java
+++ Oms-base-api/src/main/java/com/work/shop/oms/api/bean/OrderDetailInfo.java
@@ -479,6 +479,18 @@ public class OrderDetailInfo  implements Serializable{
 	 * 盈合ID
 	 */
 	private String boId;
+	/**
+	 * 团购ID
+	 */
+	private Integer groupId;
+
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
 
 	public int getOrderType() {
         return orderType;
